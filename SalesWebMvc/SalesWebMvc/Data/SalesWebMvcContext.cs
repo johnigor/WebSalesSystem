ad69525bf43f063063fca5d7017ef7dfc2536f9e
John Hansen <JOHN00IGOR@GMAIL.COM>
2022-06-15 10:33:09 -0300

    Other entities and second migration

--- SalesWebMvc/SalesWebMvc/Data/SalesWebMvcContext.cs
+++ SalesWebMvc/SalesWebMvc/Data/SalesWebMvcContext.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using SalesWebMvc.Models;
 
 namespace SalesWebMvc.Data
@@ -14,6 +10,8 @@ namespace SalesWebMvc.Data
         {
         }
 
-        public DbSet<SalesWebMvc.Models.Department> Department { get; set; }
+        public DbSet<Department> Department { get; set; }
+        public DbSet<Seller> Seller { get; set; }
+        public DbSet<SalesRecord> SalesRecord { get; set; }
     }
 }
